afd390d21cd96d9ce573deae0128f5ad200472e3
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-02-27 12:03:20 +1000

    more time

--- test_horizon_core_up.go
+++ test_horizon_core_up.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-const timeout = 6 * time.Minute
+const timeout = 60 * time.Minute
 
 type Root struct {
 	CoreSupportedProtocolVersion int32 `json:"core_supported_protocol_version"`
